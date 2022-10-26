e434e05559143f45329b1f076d353c8b41c9ca27
Mahbub-Auliaj <mahbubauliarahman@gmail.com>
2022-10-26 10:22:25 +0700

    Create Database

---
+++ app/Http/Controllers/BidangKategoriController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BidangKategori;
+use Illuminate\Http\Request;
+
+class BidangKategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BidangKategori  $bidangKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BidangKategori $bidangKategori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BidangKategori  $bidangKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BidangKategori $bidangKategori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BidangKategori  $bidangKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BidangKategori $bidangKategori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BidangKategori  $bidangKategori
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BidangKategori $bidangKategori)
+    {
+        //
+    }
+}
---
+++ app/Models/BidangKategori.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BidangKategori extends Model
+{
+    use HasFactory;
+}
